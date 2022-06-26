be0e07dde2dbca01f5e0c7fafafc5e6f0aae075c
Alexis Hernandez <alexis22229@gmail.com>
2022-06-25 22:21:30 -0600

    Update index to match the original website

--- src/main/scala/scalajsGames/Main.scala
+++ src/main/scala/scalajsGames/Main.scala
@@ -6,11 +6,6 @@ import games.*
 
 @main
 def ScalajsGames(): Unit = {
-  dom.document.querySelector("#app").innerHTML = """
-    <h1>Hello Scala.js</h1>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-    """
-
   val asteroids = new GameHolder("asteroids", Asteroids.apply)
   val astrolander = new GameHolder("astrolander", AstroLander.apply)
   val snake = new GameHolder("snake", Snake.apply)
