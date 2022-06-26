609a1c5e983d82de8236d0baa7595c03f2b351e4
Alexis Hernandez <alexis22229@gmail.com>
2022-06-25 22:13:15 -0600

    AstroLander works!

--- src/main/scala/scalajsGames/Main.scala
+++ src/main/scala/scalajsGames/Main.scala
@@ -12,12 +12,11 @@ def ScalajsGames(): Unit = {
     """
 
   val asteroids = new GameHolder("asteroids", Asteroids.apply)
-//  val astrolander = new GameHolder("astrolander", AstroLander.apply)
+  val astrolander = new GameHolder("astrolander", AstroLander.apply)
   val snake = new GameHolder("snake", Snake.apply)
   val pong = new GameHolder("pong", Pong.apply)
   val bricks = new GameHolder("bricks", BrickBreaker.apply)
   val tetris = new GameHolder("tetris", Tetris.apply)
-//  val games = Seq(asteroids, astrolander, snake, pong, bricks, tetris)
-  val gameList = Seq(asteroids, snake, pong, bricks, tetris)
+  val gameList = Seq(asteroids, astrolander, snake, pong, bricks, tetris)
   dom.window.setInterval(() => gameList.foreach(_.update()), 15)
 }
